5f13494fbb68863758f34db8f787f7426442d928
Snagah <152746673+Snagah@users.noreply.github.com>
2025-07-06 15:42:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  'https://https://byeaovijxqxgdybaxbnc.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5ZWFvdmlqeHF4Z2R5YmF4Ym5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MDYwMzQsImV4cCI6MjA2NzM4MjAzNH0.AdAVgyGeGqjer1C96QvfeXI2NHhJmSJiqxT8rOd0jNw'
-);
+import { supabase } from './src_supabase.js';
 
 const TAGS = [
   'Bitcoin', 'Crypto', 'Runes', 'DeFi', 'Market',
