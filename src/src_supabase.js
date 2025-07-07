1377c922c02086c614f8c67363f2e3601eb4d200
Snagah <152746673+Snagah@users.noreply.github.com>
2025-07-07 15:41:49 +0200

    Update src_supabase.js

--- src/src_supabase.js
+++ src/src_supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://YOUR_SUPABASE_URL.supabase.co';
-const supabaseKey = 'YOUR_PUBLIC_ANON_KEY';
+const supabaseUrl = 'https://byeaovijxqxgdybaxbnc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5ZWFvdmlqeHF4Z2R5YmF4Ym5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MDYwMzQsImV4cCI6MjA2NzM4MjAzNH0.AdAVgyGeGqjer1C96QvfeXI2NHhJmSJiqxT8rOd0jNw';
 export const supabase = createClient(supabaseUrl, supabaseKey);
